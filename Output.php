71e216b14113321f2f00083966469593f2efa72d
Ryan12439 <35814800+Ryan12439@users.noreply.github.com>
2018-03-12 13:13:23 -0500

    Update Output.php

--- Output.php
+++ Output.php
@@ -1,1 +1,18 @@
 
+<!DOCTYPE html>
+<html>
+<head>
+<title>QR Generator</title>
+</head>
+<body>
+<h1>QR Genrator</h1>
+  <img src="https://chart.googleapis.com/chart?cht=qr&chs=150x150&choe=UTF-8&chld=H&chl=<?php echo (int)$_POST['url']; ?>
+<form action="/output.php">
+<textarea name="url">
+URL
+</textarea>
+</br>
+<input type="submit" />
+</form>
+  </body>
+</html>
